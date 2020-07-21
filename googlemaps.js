052c88fe5085931e11b5b54f3580ee1c7eab07af
gaspar91 <andreiafg.91@gmail.com>
2020-07-21 10:35:16 +0000

    added lcation coordinates for the rest of the locations

--- googlemaps.js
+++ googlemaps.js
@@ -10,6 +10,8 @@ function initMap() {
   });
 }
 
+//My Locations
+
 function myRestaurantLocation() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
@@ -146,6 +148,278 @@ function myCoffeeLocation() {
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
+function myAccomodationLocation() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: {
+      lat: 59.911491,
+      lng: 10.757933,
+    },
+  });
+
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+  //coffee shops locations
+
+  var locations = [
+    {//Saga Hotel Oslo Central
+      lat: 59.9100318,
+      lng: 10.741466,
+    },
+    {//Thon Hotel Rosenkrantz Oslo
+      lat: 59.9156194,
+      lng: 10.7404806,
+    },
+    {//Central City Apartments
+      lat: 59.9136725,
+      lng: 10.7774321,
+    },
+    {// Center rooms
+      lat: 59.9092528,
+      lng: 10.7666435,
+    },
+    {// Citybox Oslo
+      lat: 59.9103456,
+      lng: 10.7472907,
+    },
+    {// Comfort Hotel Xpress Central Station
+      lat: 59.9106631,
+      lng: 10.748712,
+    },
+    {// P-Hotel Oslo
+      lat: 59.9144276,
+      lng: 10.7421132,
+    },
+    {// Saga Hotel Oslo Central
+      lat: 59.9100318,
+      lng: 10.741466,
+    },
+    {// Comfort Hotel Xpress Youngstorget
+      lat: 59.9164346,
+      lng: 10.749522,
+    },
+    {// Comfort Hotel Karl Johan
+      lat: 59.9118434,
+      lng: 10.7461162,
+    }
+  ];
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+function myActivitiesLocation() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: {
+      lat: 59.911491,
+      lng: 10.757933,
+    },
+  });
+
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+  //coffee shops locations
+
+  var locations = [
+    {//Friluftshuset
+      lat: 59.9012828,
+      lng: 10.7524521,
+    },
+    {//Oslo Visitor Center
+      lat: 59.9108958,
+      lng: 10.7508731,
+    },
+    {//Viking Biking & Viking Hiking
+      lat: 59.9106119,
+      lng: 10.7412764,
+    },
+    {// Norway Yacht Charter
+      lat: 59.9104695,
+      lng: 10.7316016,
+    },
+    {// Grünerløkka Minigolf Park
+      lat: 59.9184768,
+      lng: 10.7588392,
+    },
+    {// Free Walking Tours Oslo
+      lat: 59.9111335,
+      lng: 10.7502324,
+    },
+    {// Operahuset Oslo
+      lat: 59.9074885,
+      lng: 10.7531275,
+    },
+    {// Akershus Festning
+      lat: 59.907586,
+      lng: 10.7370841,
+    },
+    {// Norway Resistance Museum
+      lat: 59.9077956,
+      lng: 10.7356282,
+    },
+    {// Damstredet and Telthusbakken
+      lat: 59.9200554,
+      lng: 10.7475185,
+    }
+  ];
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+function myParksLocation() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: {
+      lat: 59.911491,
+      lng: 10.757933,
+    },
+  });
+
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+  //coffee shops locations
+
+  var locations = [
+    {//Frognerparken
+      lat: 59.9264583,
+      lng: 10.7034734,
+    },
+    {//Slottsparken
+      lat: 59.917625,
+      lng: 10.730332,
+    },
+    {//Vigelandsparken
+      lat: 59.927029,
+      lng: 10.700865,
+    },
+    {// Ekebergparken Skulpturpark
+      lat: 59.8995816,
+      lng: 10.7611578,
+    },
+    {// Tjuvholmen Sculpture Park
+      lat: 59.9063176,
+      lng: 10.7217175,
+    },
+    {// Sofienberg Park
+      lat: 59.9230071,
+      lng: 10.764023,
+    },
+    {// Middelalderparken
+      lat: 59.9047091,
+      lng: 10.7625675,
+    },
+    {// Tøyen Park
+      lat: 59.9171009,
+      lng: 10.7772202,
+    },
+    {// Studenterlunden Park
+      lat: 59.9147141,
+      lng: 10.734458,
+    },
+    {// Vaterlandsparken
+      lat: 59.9131819,
+      lng: 10.7570827,
+    }
+  ];
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+function myBarsLocation() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: {
+      lat: 59.911491,
+      lng: 10.757933,
+    },
+  });
+
+  var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+  //coffee shops locations
+
+  var locations = [
+    {//Eight
+      lat: 59.9136839,
+      lng: 10.7399639,
+    },
+    {//Angst
+      lat: 59.9139799,
+      lng: 10.7487005,
+    },
+    {//Magic Ice Bar
+      lat: 59.9154592,
+      lng: 10.737899,
+    },
+    {// HIMKOK Storgata Destilleri
+      lat: 59.9140704,
+      lng: 10.7514605,
+    },
+    {// Aku-Aku Tiki Bar
+      lat: 59.9248815,
+      lng: 10.7594508,
+    },
+    {//Last Train
+      lat: 59.9147255,
+      lng: 10.7365552,
+    },
+    {// Crow Bar & Brewery
+      lat: 59.9171972,
+      lng: 10.7534148,
+    },
+    {// Brewgata
+      lat: 59.9140818,
+      lng: 10.7552122,
+    },
+    {// Bar Boca
+      lat: 59.9254338,
+      lng: 10.759332,
+    },
+    {// Summit Bar
+      lat: 59.9187185,
+      lng: 10.7339001,
+    }
+  ];
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
 
 let restaurants = [
   "A. Den Glade Gris",
@@ -159,7 +433,6 @@ let restaurants = [
   "I. Restaurant Kontrast",
   "J. Hos Thea",
 ];
-
 let coffeeshops = [
   "A. Espresso House",
   "B. Starbucks",
@@ -172,15 +445,78 @@ let coffeeshops = [
   "I. Java espresso",
   "J. Kaffebrenneriet",
 ];
+let accomodation = [
+    "A. Saga Hotel Oslo Central",
+    "B. Thon Hotel Rosenkrantz Oslo",
+    "C. Central City Apartments",
+    "D. Center rooms", 
+    "E. Citybox Oslo", 
+    "F. Comfort Hotel Xpress Central Station",
+    "G. P-Hotel Oslo",
+    "H. Saga Hotel Oslo Central",
+    "I. Comfort Hotel Xpress Youngstorget",
+    "J. Comfort Hotel Karl Johan",
+];
+let activities = [
+    "A. Friluftshuset",
+    "B. Oslo Visitor Center",
+    "C. Viking Biking & Viking Hiking",
+    "D. Norway Yacht Charter", 
+    "E. Grünerløkka Minigolf Park", 
+    "F. Free Walking Tours Oslo",
+    "G. Operahuset Oslo",
+    "H. Akershus Festning",
+    "I. Norway Resistance Museum",
+    "J. Damstredet and Telthusbakken",
+];
+let parks = [
+     "A. Frognerparken",
+     "B. Slottsparken",
+     "C. Vigelandsparken",
+     "D. Ekebergparken Skulpturpark", 
+     "E. Tjuvholmen Sculpture Park", 
+     "F. Sofienberg Park",
+     "G. Middelalderparken",
+     "H. Tøyen Park",
+     "I. Studenterlunden Park",
+     "J. Vaterlandsparken",
+];
+let bars = [
+    "A. Eight",
+    "B. Angst",
+    "C. Magic Ice Bar",
+    "D. HIMKOK Storgata Destilleri", 
+    "E. Aku-Aku Tiki Bar", 
+    "F. Last Train",
+    "G. Crow Bar & Brewery",
+    "H. Brewgata",
+    "I. Bar Boca",
+    "J. Summit Bar",
+];
 
 function myRestaurants() {
   var x = document.getElementById("demo");
   x.innerHTML = restaurants.join("<br>");
 }
-
 function myCoffee() {
   var x = document.getElementById("demo");
   x.innerHTML = coffeeshops.join("<br>");
+}
+function myAccomodation(){
+      var x = document.getElementById("demo");
+      x.innerHTML = accomodation.join("<br>");
+}
+function myActivities(){
+    var x = document.getElementById("demo");
+    x.innerHTML = activities.join("<br>");
+}
+function myParks(){
+    var x = document.getElementById("demo");
+    x.innerHTML = parks.join("<br>");
+}
+function myBars(){
+    var x = document.getElementById("demo");
+    x.innerHTML = bars.join("<br>");
 }
 
 /*function initMap(){ 
